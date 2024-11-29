acd307f0036d7bfd57d72043fca14b3f868b498a
Davi Guilherme <dvgdavi@gmail.com>
2024-11-28 23:29:13 -0300

    feat: adiciona função somaNumeros para somar dois números

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 def print_mensagem_certa():
     print("Esta é a mensagem personalizada certa!")
 
+def somaNumeros(a,b):
+    return a+b
+
 print("Hello World!")
+print(f"A soma de 6 + 12 é: {somaNumeros(6, 12)}")
 print_mensagem_certa()
+
 
